e9811acf6a3c7935568dfe32815613d28e4582bf
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-29 09:11:50 +0200

    Improved fields

--- templates/content/text.php
+++ templates/content/text.php
@@ -1,4 +1,4 @@
-<div class="grid mod-guide-text">
+<div class="grid mod-guide-text" <?php if (isset($content['toggle_key']) && !empty($content['toggle_key'])) : ?>data-mod-guide-toggle-key-content="<?php echo $content['toggle_key']; ?>"<?php endif; ?>>
     <div class="grid-xs-12">
         <article>
             <?php echo $content['text']; ?>
