c9da75741ffc4298ca309c74cc317c84baac312d
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-09 11:31:15 +0200

    Required checkboxes

--- templates/content/checkboxes.php
+++ templates/content/checkboxes.php
@@ -3,7 +3,9 @@
         <div class="form-group">
             <?php foreach ($content['checkboxes'] as $checkbox) : ?>
                 <label>
-                    <input type="checkbox" data-mod-guide-toggle-key="<?php echo $checkbox['key']; ?>"> <?php echo $checkbox['label']; ?>
+                    <input type="checkbox" data-mod-guide-toggle-key="<?php echo $checkbox['key']; ?>" <?php if (isset($checkbox['required']) && $checkbox['required']) : ?>checked disabled<?php endif; ?>>
+                    <?php echo $checkbox['label']; ?>
+                    <?php if (isset($checkbox['required']) && $checkbox['required']) : ?><span class="mod-guide-checkboxes-required">(<?php _e('required', 'modularity-guides'); ?>)</span><?php endif; ?>
                 </label>
             <?php endforeach; ?>
         </div>
