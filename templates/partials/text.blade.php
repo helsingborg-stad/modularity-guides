68617ed7286a47a548ba385f19b552649a715eea
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-05-24 08:37:21 +0200

    Use correct toggle key variable for text template

--- templates/partials/text.blade.php
+++ templates/partials/text.blade.php
@@ -1,4 +1,4 @@
-<div class="grid mod-guide-text" {!! isset($item['toggle_key']) && !empty($item['toggle_key']) ? 'data-mod-guide-toggle-key-content="' . $item['toggle_key'] . '"' : '' !!}>
+<div class="grid mod-guide-text" {!! isset($content['toggle_key']) && !empty($content['toggle_key']) ? 'data-mod-guide-toggle-key-content="' . $content['toggle_key'] . '"' : '' !!}>
     <div class="grid-xs-12">
         <article>
             {!! $content['text'] !!}
