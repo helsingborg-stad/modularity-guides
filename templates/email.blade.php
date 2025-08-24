1adf5d051093bb6fbc69d0c74205c64c416e5141
Petter Andersson <petter.andersson@helsingborg.se>
2025-08-24 20:46:24 +0000

    email

--- templates/email.blade.php
+++ templates/email.blade.php
@@ -1,55 +1,58 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <style>
-            body {
-                font-family: Roboto, Arial, sans-serif;
-                font-size: 14px;
-            } 
-            div {
-                border: 1px solid black;
-                border-top-width: 6px;
-                border-radius: 6px;
-                padding: 16px;
-                margin-bottom: 16px;
-                table {
-                    width: 100%;
-                    margin-top: 12px;
-                }
-                td {
-                    vertical-align: top;
-                    padding: 0 0 8px 0;
-                }
-                td:last-child {
-                    text-align: right;
-                }
+    <style>
+        body {
+            font-family: Roboto, Arial, sans-serif;
+            font-size: 14px;
+        }
+
+        div.widget {
+            border: 1px solid black;
+            border-top-width: 6px;
+            border-radius: 6px;
+            padding: 16px;
+            padding-top: 0;
+            margin-bottom: 16px;
+
+            div.action {
+                margin-top: 12px;
             }
-        </style>
+
+            table {
+                width: 100%;
+                margin-top: 12px;
+            }
+
+            input {
+                padding: 8;
+            }
+
+            td:last-child {
+                text-align: right;
+            }
+        }
+    </style>
     </head>
     <body>
         <p>{{ $lang['email_intro'] }}</p>
         @foreach ($content as $group => $items)
-        <div>
-            <h4>{{ $group }}</h4>
-            <table>
-                <tbody>
+        <div class="widget">
+            <h3>{{ $group }}</h3>
                 @foreach ($items as $item)
                     @if (isset($item['link_text']) && !empty($item['link_text']))
-                        <tr>
-                            <td>
+                        @php
+                            $link = '';
+                            if(isset($item['link_url']) && !empty($item['link_url'])) {
+                                $link = ' <a href="' . $item['link_url'] . '" target="_blank">' . $lang['read_more'] . '</a>';
+                            }
+                        @endphp
+                            <div class="action">
                                 <input type="checkbox" id="{{$item['id']}}" />
-                                <label for="{{$item['id']}}">{{ $item['link_text'] }}</label>
-                            </td>
-                            <td>
-                                @if (isset($item['link_url']) && !empty($item['link_url']))
-                                    <a href="{{ $item['link_url'] }}" target="_blank">{{ $lang['read_more'] }}</a>
-                                @endif
-                            </td>
-                        </tr>
+                                <label for="{{$item['id']}}">{!! $item['link_text'] . $link !!}</label>
+                            </div>
                     @endif
                 @endforeach
-                </tbody>
-            </table>
         </div>
         @endforeach
     </body>
