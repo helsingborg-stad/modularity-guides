26c921a4494a4afa67662c98170a5e16d4d9cc44
Thor Brink <thor.brink@helsingborg.se>
2025-10-28 09:57:12 +0000

    fix!: align with municipio 6.0 module registration

--- source/php/App.php
+++ source/php/App.php
@@ -7,7 +7,7 @@ class App
     public function __construct()
     {
         add_action(
-            'plugins_loaded',
+            'init',
             function () {
                 if (function_exists('modularity_register_module')) {
                     modularity_register_module(
