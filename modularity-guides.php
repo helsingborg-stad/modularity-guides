4eea6284ac6519e6fdf5290ce13ddb8779c88c85
GitHub Action <>
2025-12-10 14:17:35 +0000

    build: update package file versions to 5.0.4

--- modularity-guides.php
+++ modularity-guides.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Modularity Guides
  * Plugin URI:        (#plugin_url#)
  * Description:       A addon for Modularity to create guides
- * Version: 5.0.2
+ * Version: 5.0.4
  * Author:            Kristoffer Svanmark
  * Author URI:        (#plugin_author_url#)
  * License:           MIT
