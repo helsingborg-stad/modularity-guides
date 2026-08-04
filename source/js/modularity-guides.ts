5032f66e00697641f98b018069bbf093d0ad35ed
Petter Andersson <petter.andersson@helsingborg.se>
2026-08-04 10:08:38 +0000

    fix review comment

--- source/js/modularity-guides.ts
+++ source/js/modularity-guides.ts
@@ -31,7 +31,7 @@ function handlePrevNextClick(e: Event) {
 		const targetStep = isNext ? currentStep + 1 : currentStep - 1;
 		const targetSection = currentGuide?.querySelector(`[${ATTRIBUTE_STEP}="${targetStep}"]`);
 
-		currentGuide?.querySelectorAll('[data-guide-step]').forEach((elem) => {
+		currentGuide?.querySelectorAll(`[${ATTRIBUTE_STEP}]`).forEach((elem) => {
 			elem.removeAttribute(SELECTOR_ACCORDION_TOGGLE);
 		});
 		targetSection?.setAttribute(SELECTOR_ACCORDION_TOGGLE, '');
